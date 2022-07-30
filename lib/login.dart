977b47766b3616e14cbdac7a5d4166fc6c34ebce
samamohamedd02 <samamohamedd02@gmail.com>
2022-07-31 01:33:33 +0200

    newfils

--- lib/login.dart
+++ lib/login.dart
@@ -11,7 +11,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title:Text( "sama"),
+      ),
     );
   }
 }
