3a5b50c2b1857f480eceb54384f4b7720babae34
mohamedelshamy6 <elshamy.mohamed13@yahoo.com>
2022-07-31 13:34:22 +0200

    change name

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:untitled3/login.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(const MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
