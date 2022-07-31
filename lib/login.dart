a3a0e31150693fd4541b846750c8a136a6d9f715
samamohamedd02 <samamohamedd02@gmail.com>
2022-07-31 06:03:38 +0200

    newfils

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -10,6 +10,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool pass = true;
+  var url = "https://www.facebook.com/";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,97 +24,171 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0,
       ),
       body: SingleChildScrollView(
-       child:Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(0, 10, 310, 10),
-            child: const Text(
-              "phone number",
-              style: TextStyle(fontSize: 20, color: Colors.black26),
-            ),
-          ),
-          Container(
-              margin: const EdgeInsets.fromLTRB(15, 10, 0, 0),
-              width: 470,
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        const BorderSide(width: 2, color: Colors.black26),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  hintText: "+375 29 123 45 67 ",
-                ),
-                keyboardType: TextInputType.phone,
-              )),
-          Container(
-            margin: const EdgeInsets.fromLTRB(0, 10, 350, 10),
-            child: const Text(
-              "Password",
-              style: TextStyle(fontSize: 20, color: Colors.black26),
-            ),
-          ),
-          Container(
-              margin: const EdgeInsets.fromLTRB(15, 10, 0, 0),
-              width: 470,
-              child: TextField(
-                obscureText: pass,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        pass = !pass;
-                      });
-                    },
-                    icon: Icon(pass ? Icons.visibility : Icons.visibility_off),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        const BorderSide(width: 2, color: Colors.black26),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  hintText: "Enter password ",
-                ),
-                keyboardType: TextInputType.visiblePassword,
-              )),
-          Container(
-              margin: const EdgeInsets.fromLTRB(15, 20, 0, 0),
-              color: Colors.deepOrange,
-              width: 470,
-              height: 35,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
-                ),
-                onPressed: () {},
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 310, 10),
                 child: const Text(
-                  "sign in",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  "phone number",
+                  style: TextStyle(fontSize: 20, color: Colors.black26),
                 ),
-              )),
-          Container(
-            margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-            child: TextButton(
-              onPressed: () {},
-              child: const Text("Recover Password",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.deepOrange)),
-            ),
+              ),
+              Container(
+                  margin: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+                  width: 470,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(width: 2, color: Colors.black26),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      hintText: "+375 29 123 45 67 ",
+                    ),
+                    keyboardType: TextInputType.phone,
+                  )),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 350, 10),
+                child: const Text(
+                  "Password",
+                  style: TextStyle(fontSize: 20, color: Colors.black26),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+                  width: 470,
+                  child: TextField(
+                    obscureText: pass,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            pass = !pass;
+                          });
+                        },
+                        icon: Icon(
+                            pass ? Icons.visibility : Icons.visibility_off),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(width: 2, color: Colors.black26),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      hintText: "Enter password ",
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                  )),
+              Container(
+                  margin: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+                  color: Colors.deepOrange,
+                  width: 470,
+                  height: 35,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.deepOrange),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "sign in",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  )),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Recover Password",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.deepOrange)),
+                ),
+              ),
+              const SizedBox(
+                width: 150,
+                child: Divider(
+                  color: Colors.deepOrange,
+                  height: 5,
+                  thickness: 2,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+              ),
+              Container(
+                width: 470,
+                margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: Row(children: const <Widget>[
+                  Expanded(child: Divider(color: Colors.black26)),
+                  Text(
+                    "Or continue with",
+                    style: TextStyle(color: Colors.black26, fontSize: 15),
+                  ),
+                  Expanded(child: Divider(color: Colors.black26)),
+                ]),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: (() {
+                        setState(() async {
+                          await launch(url);
+                        });
+                      }),
+                      child: CircleAvatar(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.white,
+                        radius: 30,
+                        child: Image.asset(
+                          "assets/images/face.jpg",
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                    CircleAvatar(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.white,
+                      radius: 30,
+                      child: Image.asset(
+                        "assets/images/apple.jpg",
+                        width: 80,
+                        height: 100,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Haven't an account ?",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.black26),
+                    ),
+                    Text(
+                      "Sing up ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.deepOrange),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(
-            width: 150,
-            child: Divider(
-              color: Colors.deepOrange,
-              height: 5,
-              thickness: 2,
-              indent: 0,
-              endIndent: 0,
-            ),
-          ),
-        ],
-      ),),
+        ),
+      ),
     );
   }
 }
